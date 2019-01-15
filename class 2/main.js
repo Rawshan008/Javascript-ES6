04ebbbfeb93a1f8579761c14df8fcd937670a938
Rawshan008 <pust.love008@gmail.com>
2019-01-16 01:15:57 +0600

    class 2 ES6 Special featuees Template String

---
+++ class 2/main.js
@@ -0,0 +1,10 @@
+
+const FirstName = "Rawshan";
+const LastName = "Ali";
+const age = "25";
+
+function showData(){
+    return `I am ${FirstName} ${LastName}. I am ${age} years Old`;
+}
+console.log(showData());
+
