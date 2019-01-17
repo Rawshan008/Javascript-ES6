c0a6e9b0dc5102d4299ceb0bf5d7a5161b54aa83
Rawshan008 <pust.love008@gmail.com>
2019-01-17 13:18:24 +0600

    class 6 arraw function and class 7 map and forEach function

---
+++ class 6/main.js
@@ -0,0 +1,7 @@
+
+// Arrow function
+
+const add = (number) => "I am rawshan");
+console.log(add(5));
+
+
---
+++ class 7/main.js
@@ -0,0 +1,21 @@
+// marks 
+const marks = [80, 85, 90, 65];
+// this is and simple function of map
+// const doubleMarks = marks.map(mark => mark*2);
+// console.log(doubleMarks);
+
+// const doubleMarks = marks.map((mark, i) => {
+//     mark+6;
+//     console.log(i);
+//     mark = mark*2;
+//     console.log(mark);
+// });
+
+// foreach loop for js
+marks.forEach(function(item){
+    item = item*2;
+    console.log(item);
+});
+
+
+
